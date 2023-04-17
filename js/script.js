1175ede6ba7788d0e8b3558544fe24c23a9d20ef
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-04-17 08:41:18 +0200

    played around with objects

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+const learning = {};
+  learning.topic = "JS";
+  learning.learningGoals = ["build projects", "learn React", "keep coding"];
+  learning.catagory = "front end developmeny";
+  learning.topicImportance = "high";
+
+
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1, 1);
+
+console.log(learning);
+
+topicElement.innerText = `✔️ I'm learning ${learning.topic}`;
+topicElement.classList.remove("hide");
+
+countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals`;
+countElement.classList.remove("hide");
